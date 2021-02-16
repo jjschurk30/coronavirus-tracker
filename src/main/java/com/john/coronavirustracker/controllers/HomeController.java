e51f5fdd9b05987d330dc85775f431109a4d5f47
John Schurk <johnschurk@yahoo.com>
2021-02-16 08:59:31 -0600

    First Commit

---
+++ src/main/java/com/john/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.john.coronavirustracker.controllers;
+
+import com.john.coronavirustracker.models.LocationStats;
+import com.john.coronavirustracker.services.CoronavirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronavirusDataService coronavirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronavirusDataService.getAllstats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
